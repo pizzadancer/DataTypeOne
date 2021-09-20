0fadc55fabaea2f818a8a746066cade8d6d3b336
dlm <dannyleematthew@gmail.com>
2021-09-19 20:58:40 -0400

    Add project files.

---
+++ InputOutput/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace InputOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declarations
+            string input;
+            double length;
+            double height;
+            double area;    // length * width
+
+            // Get Length from User
+            Console.WriteLine(">>>Area Calculator<<<");
+            Console.WriteLine("Please enter the length of the rectangle. ");
+            input = Console.ReadLine();
+            length = double.Parse(input);
+
+            // Get Height from User
+            Console.WriteLine("Please enter the height of the rectangle. ");
+            input = Console.ReadLine();
+            height = double.Parse(input);
+
+            // Calculate Area
+            Console.WriteLine("Great. Calculating Area. Hit ENTER to continue.");
+            Console.ReadLine();
+            area = length * height;
+            Console.WriteLine("The total area for your rectangle is: " + area);
+            Console.WriteLine("\nArea calculated. Goodbye.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MoreNumericTypes/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace MoreNumericTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declarations
+            string input;
+            double milesDriven;
+            double gasConsumed;
+            double milesPerGallon;
+
+            Console.WriteLine(">>>Gas Mileage Calculator<<<");
+
+            // Get milesDriven from User
+            Console.WriteLine("Please enter how far you drove (in miles). ");
+            input = Console.ReadLine();
+            milesDriven = double.Parse(input);
+
+            // Get gasConsumed from User
+            Console.WriteLine("Please enter how much gas you used (in gallons). ");
+            input = Console.ReadLine();
+            gasConsumed = double.Parse(input);
+
+            // Calculate milesPerGallon
+            Console.WriteLine("Great. Calculating milesPerGallon. Hit ENTER to continue.");
+            Console.ReadLine();
+
+            milesPerGallon = milesDriven / gasConsumed;
+            Console.WriteLine("The total miles-per-gallon you had for your drive: " + milesPerGallon + " miles-per-gallon.");
+            Console.WriteLine("\nMiles-per-gallon Calculated. \nGoodbye.");
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
